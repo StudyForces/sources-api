1d5b6359b8666794639afa86c98f2ba57609d21d
Pavel Kasila <pavel.kasila@gmail.com>
2022-06-02 17:05:31 +0300

    Add RestConfig and make repository for SourceUploadRect internal one

---
+++ src/main/java/com/studyforces/sourcesapi/configuration/RestConfig.java
@@ -0,0 +1,20 @@
+package com.studyforces.sourcesapi.configuration;
+
+import com.studyforces.sourcesapi.models.*;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Component
+public class RestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.setDefaultMediaType(MediaType.APPLICATION_JSON);
+        config.useHalAsDefaultJsonMediaType(false);
+        config.exposeIdsFor(SourceUpload.class);
+        config.exposeIdsFor(SourceUploadRect.class);
+    }
+}
--- src/main/java/com/studyforces/sourcesapi/repositories/SourceUploadRectRepository.java
+++ src/main/java/com/studyforces/sourcesapi/repositories/SourceUploadRectRepository.java
@@ -1,7 +1,7 @@
 package com.studyforces.sourcesapi.repositories;
 
 import com.studyforces.sourcesapi.models.SourceUploadRect;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
-public interface SourceUploadRectRepository extends JpaRepository<SourceUploadRect, Long> {
+public interface SourceUploadRectRepository extends Repository<SourceUploadRect, Long> {
 }
