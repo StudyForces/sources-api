86ce7ffa76bb6b4e7dc0f0453fae55880a1e1e63
Pavel Kasila <pavel.kasila@gmail.com>
2022-06-06 13:05:28 +0300

    #2: add consumer to handle messages from OCR microservice through RabbitMQ, save updated SourceUpload in OCRService instead of OCRController

--- src/main/java/com/studyforces/sourcesapi/models/SourceUploadRectStatus.java
+++ src/main/java/com/studyforces/sourcesapi/models/SourceUploadRectStatus.java
@@ -1,5 +1,5 @@
 package com.studyforces.sourcesapi.models;
 
 public enum SourceUploadRectStatus {
-    PENDING, FAILED, SUCCEED
+    PENDING, DONE
 }
---
+++ src/main/java/com/studyforces/sourcesapi/services/messages/OCRDataText.java
@@ -0,0 +1,13 @@
+package com.studyforces.sourcesapi.services.messages;
+
+public class OCRDataText {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
