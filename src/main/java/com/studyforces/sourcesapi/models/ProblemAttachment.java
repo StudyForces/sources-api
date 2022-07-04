b1dddd74c83beb06daf6d9d917faf1fd996e6812
Pavel Kasila <pavel.kasila@gmail.com>
2022-07-04 14:57:24 +0300

    Add metadata field to ProblemAttachment

--- src/main/java/com/studyforces/sourcesapi/models/ProblemAttachment.java
+++ src/main/java/com/studyforces/sourcesapi/models/ProblemAttachment.java
@@ -2,6 +2,7 @@ package com.studyforces.sourcesapi.models;
 
 import javax.persistence.Basic;
 import javax.persistence.Embeddable;
+import javax.persistence.Lob;
 
 @Embeddable
 public class ProblemAttachment {
@@ -14,5 +15,16 @@ public class ProblemAttachment {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    @Lob
+    private Object metadata;
+
+    public Object getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Object metadata) {
+        this.metadata = metadata;
     }
 }
