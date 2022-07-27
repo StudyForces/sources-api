77230205695a73731afbfc9e9a6644a9211e17ec
Pavel Kasila <pavel.kasila@gmail.com>
2022-07-27 18:56:13 +0300

    Fixes

--- src/main/java/com/studyforces/sourcesapi/models/solve/ProblemSolve.java
+++ src/main/java/com/studyforces/sourcesapi/models/solve/ProblemSolve.java
@@ -2,18 +2,19 @@ package com.studyforces.sourcesapi.models.solve;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Embeddable;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
 @Embeddable
 public class ProblemSolve {
+    @Enumerated(EnumType.STRING)
     @Column(name = "solve_type")
     private ProblemSolveType type;
     @ElementCollection
     private List<ProblemSolveVariant> variants;
+    @Column(name = "solve_corrent")
     private ProblemSolveVariant correct;
+    @Column(name = "solve_formula")
     private String formula;
 }
--- src/main/java/com/studyforces/sourcesapi/models/solve/ProblemSolveVariant.java
+++ src/main/java/com/studyforces/sourcesapi/models/solve/ProblemSolveVariant.java
@@ -4,13 +4,19 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Data
 @Embeddable
 public class ProblemSolveVariant {
+    @Enumerated(EnumType.STRING)
     @Column(name = "variant_type")
     private ProblemSolveVariantType type;
+    @Column(name = "variant_number")
     private Float number;
+    @Column(name = "variant_string")
     private String string;
+    @Column(name = "variant_index")
     private Integer index;
 }
